ec515b9240d17f2d264547f52bb97f81905db9f2
Hong Jiang <jianghong@xd.com>
2022-07-02 15:28:08 +0800

    initial login page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
+import { CssVarsProvider } from '@mui/joy/styles';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <CssVarsProvider><Component {...pageProps} /></CssVarsProvider>
 }
 
 export default MyApp
